09cc582559de5599ed386626d136fa3a3564d494
Kevin9842 <kevinliriano200816@gmail.com>
2026-08-19 22:01:13 -0400

    Fix

--- src/server.js
+++ src/server.js
@@ -2,6 +2,14 @@ const app = require('./app');
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
+const server = app.listen(PORT, () => {
   console.log(`Servidor ejecutándose en el puerto ${PORT}`);
+});
+
+process.on('SIGTERM', () => {
+  console.log('Señal SIGTERM recibida: Cerrando servidor HTTP de forma limpia...');
+  server.close(() => {
+    console.log('Servidor HTTP cerrado.');
+    process.exit(0);
+  });
 });
